96675c8b55dcd92dcdf33157ba54714885d9864c
endlesslupita <endlesslupita@users.noreply.github.com>
2026-03-19 01:46:17 -0700

    Add ingredient list

--- recipe-gallery/src/RecipeGallery.jsx
+++ recipe-gallery/src/RecipeGallery.jsx
@@ -5,10 +5,12 @@ function App() {
       return (
         <div key={recipe.id}>
           <h2>Meal: {recipe.title}</h2>
-          <p>Ingredients: {recipe.ingredients.map(function(ingredientList){})
+          <ul>Ingredients: {recipe.ingredients.map(function(ingredientList){
+<li key={ingredientList}>{ingredientList}</li>
+          })
                 
-                }</p>
-          <p>{recipe.image}</p>
+                }</ul>
+          <img src={recipe.image} alt={recipe.title} />
 
         </div>
       )
