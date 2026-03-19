7a8643ecdcea3f3a817729cb38c78533f31cbd80
endlesslupita <endlesslupita@users.noreply.github.com>
2026-03-19 01:17:43 -0700

    Create recipe array

---
+++ recipe-gallery/src/data/recipes.js
@@ -0,0 +1,26 @@
+const recipes = [{
+  id: 1,
+  title: "Beombeok 범벅",
+  ingredients: ["Squash", "Red beans", "Chestnuts", "Sweet Potatoes"],
+  image: "https://upload.wikimedia.org/wikipedia/commons/8/80/Beombeok.jpg"
+},
+{
+  id: 2,
+  title: "Jangguk-juk 장국죽",
+  ingredients: ["Beef", "Sesame oil", "Garlic", "Soy Sauce"],
+  image: "https://upload.wikimedia.org/wikipedia/commons/c/c3/Jangguk-juk.jpg"
+},
+{
+  id: 3,
+  title: "Soldier Stew 부대찌개",
+  ingredients: ["Spam", "Hot dogs", "Ramen", "Mushrooms"],
+  image: "https://omnivorescookbook.com/wp-content/uploads/2019/05/1403_Korean-Army-Stew-Budae-Jjigae_002.jpg"
+},
+{
+  id: 4,
+  title: "Doenjang jjigae 된장찌개",
+  ingredients: ["Tofu", "Zucchini", "Fermented soybean paste", "Red pepper flakes"],
+  image: "https://www.thatcutedish.com/wp-content/uploads/2023/08/doenjang-jjigae-korean-soybean-paste-stew-1.jpg"
+}]
+
+export default recipes
